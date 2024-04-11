3ee74aa9f5f2fe4e8ba7424e4e412f6e97af846e
AJAY KANDPAL <ajay.kandpal1998@gmail.com>
2024-04-11 21:41:40 +0530

    SignIn SignUp form, Login oage and Header

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className='absolute px-8 py-2 bg-gradient-to-b from-black z-10'>
+      <img
+        className='w-44'
+        src='https://imgs.search.brave.com/fUeSxYDQoVKpFgawp73nTPVtmWGMS1oFzJZIxLJZsE0/rs:fit:860:0:0/g:ce/aHR0cHM6Ly8xMDAw/bG9nb3MubmV0L3dw/LWNvbnRlbnQvdXBs/b2Fkcy8yMDE3LzA1/L05ldGZsaXgtTG9n/by01MDB4MjgxLnBu/Zw'
+        alt='logo'
+      />
+    </div>
+  );
+};
+
+export default Header;
